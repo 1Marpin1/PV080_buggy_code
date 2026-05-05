5cde1956864199dbbaea6fe7ae14bc20ec5df8ec
1Marpin1 <marpinwork@gmail.com>
2026-05-05 14:52:09 +0200

    fix: Fixing errors from Codacy

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,7 +1,8 @@
-import sys 
+import sys
 import os
 import yaml
 import flask
+import urllib
 
 app = flask.Flask(__name__)
 
@@ -12,23 +13,25 @@ def index():
     url = flask.request.args.get("url")
     return fetch_website(version, url)
 
-        
+
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
-class Person(object):
+class Person:
     def __init__(self, name):
         self.name = name
 
 
 def print_nametag(format_string, person):
+    """Here should be something about how it works"""
     print(format_string.format(person=person))
 
 
 def fetch_website(urllib_version, url):
+    """Here should be something about how it works"""
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
  
-    try: 
+    try:
         http = urllib.PoolManager()
         r = http.request('GET', url)
     except:
@@ -52,8 +55,8 @@ if __name__ == '__main__':
     print("3. Yaml deserialization vulnerability:")
     print("4. Use of assert statements vulnerability:")
     choice  = input("Select vulnerability: ")
-    if choice == "1": 
-        new_person = Person("Vickie")  
+    if choice == "1":
+        new_person = Person("Vickie")
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
         urlib_version = input("Choose version of urllib: ")
